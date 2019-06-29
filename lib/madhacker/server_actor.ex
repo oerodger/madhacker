0047723238f29992f00effe7ed364c2d15093562
Roman Akhmedianov <rodger@elewise.com>
2019-06-29 19:28:54 +0400

    server

--- lib/madhacker/server_actor.ex
+++ lib/madhacker/server_actor.ex
@@ -3,6 +3,14 @@ defmodule Madhacker.ServerActor do
 
   def init(state), do: {:ok, state}
 
+  def handle_call(:attack, val, { node }) do
+    if val > node.defence do
+      {:hacked}
+    else
+      {:nothacked}
+  end
+
+
   def start_link(state \\ []) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end

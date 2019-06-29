83400f83f2530021c2a66c89699e68ced3a37aa0
Evgeniy Bastrykov <vporoshok@gmail.com>
2019-06-29 13:27:27 +0400

    add user socket controller and fake match making

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ import "phoenix_html"
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import socket from "./socket"
 
 import { Terminal } from 'xterm';
 
